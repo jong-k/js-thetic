5bcda1c8e6d3c887fc8d1ed7206500b2e409cc16
jong-k <fully.charged07@gmail.com>
2024-11-25 12:15:01 +0900

    feat: div 더미 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div>hahaha</div>;
+  const getDivs = (n: number) => {
+    return (
+      <div className="flex flex-col gap-2">
+        {Array.from({ length: n }, (_, i) => (
+          <div className="h-10 w-full border-b" key={i}>
+            Div {i + 1}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return <div>{getDivs(200)}</div>;
 }
